af2a819283dc97fc89291a7e938acfc04805d4f5
ccq007 <1425122711@qq.com>
2023-12-09 22:56:36 +0800

    评论区 刘梓煊

---
+++ app/src/main/java/glue502/software/models/UploadComment.java
@@ -0,0 +1,58 @@
+package glue502.software.models;
+
+//用于显示评论区
+public class UploadComment {
+    private String postId;
+    private String userId;
+    private String text;
+    private String commentId;
+    private String time;
+
+    public UploadComment(String postId, String userId, String text, String commentId, String time) {
+        this.postId = postId;
+        this.userId = userId;
+        this.text = text;
+        this.commentId = commentId;
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+}
