471ffa67c13cc8486120f30738bdd2a0a133d8d0
ccq007 <1425122711@qq.com>
2023-12-18 15:56:14 +0800

    更新了评论区回复功能,优化了数据库

--- app/src/main/java/glue502/software/fragments/FunctionFragment.java
+++ app/src/main/java/glue502/software/fragments/FunctionFragment.java
@@ -15,6 +15,9 @@ public class FunctionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_function, container, false);
+
         return inflater.inflate(R.layout.fragment_function, container, false);
     }
 }
