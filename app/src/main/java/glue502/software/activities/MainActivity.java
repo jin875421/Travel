12565344f73c9c9e2df557ccfb555f060656b0a5
jin <jin875421@gmail.com>
2023-12-25 14:07:35 +0800

    update

--- app/src/main/java/glue502/software/activities/MainActivity.java
+++ app/src/main/java/glue502/software/activities/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.Manifest;
